a9c1342f4aee020ca1c5d79332e8434d6c066956
Felipe-Macedo-Correia <felipemacedocorreia@gmail.com>
2024-08-12 10:49:39 -0300

    separando os tabs dos stacks e melhorando o fluxo da rota

--- appProfissional/src/routes/stack.tsx
+++ appProfissional/src/routes/stack.tsx
@@ -4,23 +4,21 @@ import BemVindoScreen from '../views/bemVindo';
 import CadastroScreen from '../views/cadastro';
 import ConfirmedIdScreen from '../views/confirmeId';
 import LoadingScreen from '../views/loading';
-import loginScreen from '../views/login'
-// import mapScreen from '../../componentes/Map/map'
-import Cadastro1 from '../views/cadastroScreen2'
-
+import loginScreen from '../views/login';
+// import mapScreen from '/github/appProHelpHouse/appProfissional/componentes/Map/map';
+import Cadastro1 from '../views/cadastroScreen2';
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="bemvindo">
       <Stack.Screen name="bemvindo" component={BemVindoScreen} options={{headerShown: false}}/>
       <Stack.Screen name="cadastro" component={CadastroScreen} options={{headerShown: false}}/>
       <Stack.Screen name="confirmeid" component={ConfirmedIdScreen} options={{headerShown: false}} />
       <Stack.Screen name="loading" component={LoadingScreen} options={{headerShown: false}} />
       <Stack.Screen name="login" component={loginScreen} options={{headerShown: false}} />
-      {/* <Stack.Screen name="map" component={mapScreen} options={{headerShown: false}} /> */}
+      {/* <Stack.Screen name="map" component={mapScreen} options={{headerShown: false}} />  */}
       <Stack.Screen name="cadastro1" component={Cadastro1} options={{headerShown: false}} />
-
     </Stack.Navigator>
   );
 };
