b07d6d542261045002b3a7183228a9e8f1b4a59e
JvTorelli <joaovictorelli7@gmail.com>
2024-10-03 17:22:16 -0300

    erro da tela de login arrumado

--- appProfissional/src/views/login.tsx
+++ appProfissional/src/views/login.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import Imagens from "../../img/img";
 import { FloatingLabelInput } from 'react-native-floating-label-input';
 import { Button } from "../../componentes/Button/Button"; // Verifique se o caminho está correto
 import styles from '../css/loginCss';
-// axios e como se importasse a API da uma olhada la no arquivo axios.js
 import axios from '../../axios';
 import AsyncStorage from '@react-native-async-storage/async-storage'; // Importa AsyncStorage para armazenar o token
 
-const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
+const Login: React.FC<{ navigation: any }> = ({ navigation }) => {
     const [emailContratado, setEmailContratado] = useState('');
     const [password, setPassword] = useState('');
-    const [message, setMessage]= useState ('')
+    const [message, setMessage] = useState('');
     const [show, setShow] = useState(false);
     const [loading, setLoading] = useState(false);
-
-
 
     const handleLogin = async () => {
         if (!emailContratado || !password) {
@@ -23,42 +20,37 @@ const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
             return;
         }
 
+        setMessage(''); // Limpa a mensagem de erro
+        setLoading(true);
 
-    setLoading(true)
         try {
-            const response = await axios.post('http:///localhost:8000/api/authPro', {
+            const response = await axios.post('http://localhost:8000/api/authpro', {
                 emailContratado,
                 password,
             });
 
-              // Verifica se o login foi bem-sucedido e se o token está presente
-        if (response.data && response.data.status === 'Sucesso' && response.data.token) {
-            console.log("Token recebido:", response.data.token);
-            console.log("Seja bem vindo novamente! ")
+            if (response.data && response.data.status === 'Sucesso' && response.data.token) {
+                console.log("Token recebido:", response.data.token);
+                console.log("Seja bem-vindo novamente!");
 
-            // Armazena o token no AsyncStorage
-            await AsyncStorage.setItem('authToken', response.data.token);
-
-            // Navega para a próxima tela após login bem-sucedido
-            navigation.navigate('homeStack', { screen: 'home' });
-        } else {
-            setMessage('Credenciais incorretas, tente novamente.');
+                await AsyncStorage.setItem('authToken', response.data.token);
+                navigation.navigate('telaServico', { screen: 'telaServico' });
+            } else {
+                setMessage('Credenciais incorretas, tente novamente.');
+            }
+        } catch (error) {
+            console.error('Erro ao fazer login:', error);
+            setMessage('Erro ao fazer login. Verifique suas credenciais e tente novamente.');
+        } finally {
+            setLoading(false);
         }
-
-    } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        setMessage('Erro ao fazer login. Verifique suas credenciais e tente novamente.');
-    } finally {
-        setLoading(false); // Desativa o estado de loading
-    }
-};
+    };
 
     return (
         <View style={styles.container}>
             <Image source={Imagens.helpHouse} style={styles.help} />
 
             <View style={styles.input}>
-
                 <FloatingLabelInput
                     label=" Email "
                     value={emailContratado}
@@ -75,8 +67,7 @@ const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
                         borderLeftWidth: 5,
                         borderRightWidth: 5,
                         borderBottomWidth: 5,
-                        position:'relative',
-
+                        position: 'relative',
                     }}
                     customLabelStyles={{
                         colorFocused: '#FF8F49',
@@ -86,21 +77,16 @@ const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
                         backgroundColor: '#fff',
                         paddingHorizontal: 5,
                         color: '#FF8F49',
-                        height:'29%',
-
+                        height: '29%',
                     }}
                     inputStyles={{
                         color: '#000',
                         paddingHorizontal: 10,
-                        height:49
-
+                        height: 49,
                     }}
                     onChangeText={setEmailContratado}
-                    // onChangeText={(text)=>{form.emailContratado = text}}
-
                 />
-                 <Text style={styles.errorMessage}>{message}</Text>
-                 {/* <Text style={styles.branco}></Text>  */}
+                <Text style={styles.errorMessage}>{message}</Text>
             </View>
 
             <View style={styles.input}>
@@ -124,7 +110,6 @@ const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
                         borderRightWidth: 5,
                         borderBottomWidth: 5,
                     }}
-
                     customLabelStyles={{
                         colorFocused: '#FF8F49',
                         fontSizeFocused: 12,
@@ -138,35 +123,32 @@ const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
                         color: '#000',
                         paddingHorizontal: 10,
                     }}
-                    // onChangeText={(text)=>{form.password = text}}
-
                 />
-
             </View>
 
-            <Button
-                style={styles.button}
-                color='#004AAD'
-                variant="primary"
-                title="Entrar" 
-                //onPress={() => navigation.navigate('cadastro')} 
-
-                 onPress={handleLogin} 
-    />
-
+            {loading ? (
+                <ActivityIndicator size="large" color="#004AAD" />
+            ) : (
+                <Button
+                    style={styles.button}
+                    color='#004AAD'
+                    variant="primary"
+                    title="Entrar"
+                    onPress={handleLogin}
+                />
+            )}
 
             <View>
                 <View style={styles.conta}>
                     <Text>Ainda não tem uma conta</Text>
-                    <Text>Profissional <Text style={styles.helpText} >Help</Text><Text style={styles.houseText}>House</Text>? </Text>
+                    <Text>Profissional <Text style={styles.helpText}>Help</Text><Text style={styles.houseText}>House</Text>? </Text>
                 </View>
             </View>
 
             <Button
-                style={[styles.buttonCad, { backgroundColor: '#004AAD' }]} // Defina a cor de fundo desejada aqui
+                style={[styles.buttonCad, { backgroundColor: '#004AAD' }]}
                 variant="primary"
                 title="Cadastre-se"
-
                 onPress={() => navigation.navigate('cadastroEmail')}
             />
         </View>
@@ -174,6 +156,3 @@ const Login: React.FC<{ navigation: any,  }> = ({ navigation }) => {
 };
 
 export default Login;
-function setError(arg0: string) {
-    throw new Error('Function not implemented.');
-}
