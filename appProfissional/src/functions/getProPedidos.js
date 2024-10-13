8ae287df504e4fa48aad5e3b236a5d7cf4240f2d
Guilherme Mesquita <guimesquita.1512@gmail.com>
2024-10-13 16:57:23 -0300

    marge

--- appProfissional/src/functions/getProPedidos.js
+++ appProfissional/src/functions/getProPedidos.js
@@ -1,2 +1,17 @@
-import api from '../../axios';
+import api from "../../axios"
 
+export const getPedidosPendentes = async (setData, setLoading, setError) =>{
+setLoading(true);
+
+try{
+    const token = await AsyncStorage.getItem('token');
+    
+    const response = await api.get("/profissional/${idContratado}/pedidos");
+    setData(response?.data);
+    setLoading(false)
+}catch (e){
+    setError(true);
+    setLoading(false)
+
+}
+}
