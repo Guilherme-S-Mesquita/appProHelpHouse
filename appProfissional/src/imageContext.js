c3b46501b259a2f6f5ecf0ca743302368cca0add
Felipe macedo correia <felipemacedocorreia@gmail.com>
2024-11-16 11:38:48 -0300

    portifolio no cad feito com secesso. (bagulho chato da bixigaaa)

--- appProfissional/src/imageContext.js
+++ appProfissional/src/imageContext.js
@@ -4,9 +4,11 @@ const ImageContext = createContext();
 
 export const ImageProvider = ({ children }) => {
   const [imageUrl, setImageUrl] = useState(null);
-
+  const [portifolio1, setPortifolio1] = useState(null);
+  const [portifolio2, setPortifolio2] = useState(null);
+  const [portifolio3, setPortifolio3] = useState(null);
   return (
-    <ImageContext.Provider value={{ imageUrl, setImageUrl }}>
+    <ImageContext.Provider value={{ imageUrl, setImageUrl, portifolio1, setPortifolio1, portifolio2, setPortifolio2, portifolio3, setPortifolio3 }}>
       {children}
     </ImageContext.Provider>
   );
