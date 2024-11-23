f2d6c98006684048559f11935e1d2e1fb3852cf6
sttephany11 <rodriguesstephany76@gmail.com>
2024-11-22 21:25:11 -0300

    marge

--- appProfissional/axios.js
+++ appProfissional/axios.js
@@ -4,11 +4,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const api = axios.create({
 
 
-<<<<<<< HEAD
-    baseURL:'http://172.20.10.3:8000/api',
-=======
-    baseURL:'http://192.168.1.194:8000/api',
->>>>>>> d77c95869d8ff628a4cc34c5d0163ac127ff7cc5
+    baseURL:'http://192.168.1.10:8000/api',
 
 
 });
